196d535fd3cb3e6c4600655669ed8e01ffd0723d
mnj <mgntprg@gmail.com>
2020-06-14 20:35:30 -0700

    commit-2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Welcome</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
