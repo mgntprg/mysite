3fee72758b293ea22a448f1837a52f60727894df
mnj <mgntprg@gmail.com>
2020-06-14 20:28:24 -0700

    Updates

---
+++ precache-manifest.4f3f21dd6364d7de7a1653229194db84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b63666c704f86bb041e4f56aff2a6b9",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "e7abe80294921482c681",
+    "url": "/mysite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2f7fa6edf61005a226d9",
+    "url": "/mysite/static/js/2.7b273cc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mysite/static/js/2.7b273cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7abe80294921482c681",
+    "url": "/mysite/static/js/main.a42e3bc2.chunk.js"
+  },
+  {
+    "revision": "af90e5ac634f8a1a9577",
+    "url": "/mysite/static/js/runtime-main.54d08cc0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mysite/static/media/logo.5d5d9eef.svg"
+  }
+]);
